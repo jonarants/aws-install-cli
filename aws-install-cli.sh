2066e2ca3d51e75a5e7c8200ad9581746db24e0b
Jonathan Vazquez Duran <jonathan.vazquezd@outlook.es>
2025-06-01 22:05:34 -0600

    Se agregaron cambios y se corrigieron lineas con errores de formato, se prueba nuevamente en dev

--- aws-install-cli.sh
+++ aws-install-cli.sh
@@ -32,7 +32,7 @@ echo -e "\e[1;92mInstalación completada de manera exitosa\e[0m"
 echo -e"# $SCRIPT_NAME v${SCRIPT_VERSION} - Escrito por \e[1;91m${AUTHOR}\e[0m"
 echo "# Github: ${REPO_URL}"
 echo "==============================================================================="
-echo -e "Comenzando la instalación de \e[1;93mDocker Engine\e[0m en \e[1;92mWSL \e[0m"
+echo -e "Comenzando la instalación de \e[1;93mDocker Engine\e[0m en \e[1;92mWSL\e[0m"
 echo -e "Configurando el repo de \e[1;93mDocker\e[0m"
 echo "==============================================================================="
 
@@ -50,10 +50,10 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-echo -e "Actualizando \e[1,93mDocker\e[0m"
+echo -e "Actualizando \e[1;93mDocker\e[0m"
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo -e "Despliegue de \e[95mhello-world\e[0m"
+echo -e "Despliegue de \e[1;95mhello-world\e[0m"
 sudo docker run hello-world
 echo -e "\e[1;92mSe han instalado AWS CLI y Docker Engine en WSL de manera exitosa\e[0m"
 echo -e"# $SCRIPT_NAME v${SCRIPT_VERSION} - Escrito por \e[1;91m${AUTHOR}\e[0m"
