b42aed713e4ea9c0949ce659c36e3d329d9f2462
Jonathan Vazquez Duran <jonathan.vazquezd@outlook.es>
2025-06-01 22:13:53 -0600

    Mas correcciones al loggin del bash

--- aws-install-cli.sh
+++ aws-install-cli.sh
@@ -3,32 +3,43 @@ SCRIPT_NAME=$(basename "$0")
 SCRIPT_VERSION="1.0.2"
 REPO_URL="https://github.com/jonarants/aws-install-cli"
 AUTHOR="JonaRants"
-
+echo "======================================================================="
 echo "# $SCRIPT_NAME v${SCRIPT_VERSION} - Escrito por ${AUTHOR}"
 echo "# Github: ${REPO_URL}"
 echo -e "\e[1;92mSe inicia el proceso de instalación de AWS CLI para WSL\e[0m"
 echo "Se actualizaran los repositorios locales"
+echo "======================================================================="
 sudo apt update
 sudo apt upgrade -y
+echo "======================================================================="
 echo "Se instalara unzip"
+echo "======================================================================="
 sudo apt install unzip -y
+echo "======================================================================="
 echo "Se inicia la descarga del AWS CLI"
+echo "======================================================================="
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" || {
     echo -e "\e[1;91mError al descargar AWS CLI. Revisa tu conexión a internet\e[0m"
     exit 1
 }
-echo -e "\e[1;92Descarga exitosa\e[0m"
+echo "======================================================================="
+echo -e "\e[1;92mDescarga exitosa\e[0m"
 echo -e "\e[1;92mDescomprimiendo\e[0m"
+echo "======================================================================="
 unzip awscliv2.zip
+echo "======================================================================="
 echo "Instalando..."
 sudo ./aws/install
+echo "======================================================================="
 echo "Verificando la instalación..."
 aws --version
 echo -e "\e[1;92mInstalación verificada...\e[0m"
 echo -e "\e[1;91mRemoviendo archivos innecesarios\e[0m"
+echo "======================================================================="
 rm awscliv2.zip
 rm -rf aws
 echo -e "\e[1;92mInstalación completada de manera exitosa\e[0m"
+echo "======================================================================="
 echo -e"# $SCRIPT_NAME v${SCRIPT_VERSION} - Escrito por \e[1;91m${AUTHOR}\e[0m"
 echo "# Github: ${REPO_URL}"
 echo "==============================================================================="
@@ -49,12 +60,16 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-
+echo "======================================================================="
 echo -e "Actualizando \e[1;93mDocker\e[0m"
+echo "======================================================================="
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
+echo "======================================================================="
 echo -e "Despliegue de \e[1;95mhello-world\e[0m"
+echo "======================================================================="
 sudo docker run hello-world
+echo "======================================================================="
 echo -e "\e[1;92mSe han instalado AWS CLI y Docker Engine en WSL de manera exitosa\e[0m"
-echo -e"# $SCRIPT_NAME v${SCRIPT_VERSION} - Escrito por \e[1;91m${AUTHOR}\e[0m"
+echo -e "# $SCRIPT_NAME v${SCRIPT_VERSION} - Escrito por \e[1;91m${AUTHOR}\e[0m"
 echo "# Github: ${REPO_URL}"
+echo "======================================================================="
