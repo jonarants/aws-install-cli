0d8a9bd77115167132a957eb5a50f15dede2cd92
Jonathan Vazquez Duran <jonathan.vazquezd@outlook.es>
2025-06-01 21:52:39 -0600

    Se añade los pasos para instalar Docker Engine y confirmar con un despliegue

--- aws-install-cli.sh
+++ aws-install-cli.sh
@@ -31,3 +31,26 @@ rm -rf aws
 echo -e "\e[1;92mInstalación completada de manera exitosa\e[0m"
 echo "# $SCRIPT_NAME v${SCRIPT_VERSION} - Escrito por ${AUTHOR}"
 echo "# Github: ${REPO_URL}"
+
+echo -e "Comenzando la instalación de \e[1:93Docker Engine\e[0m en \e[1:92mWSL \e[0m"
+echo -e "Configurando el repo de \e[1;93mDocker\e[0m"
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+echo -e "Actualizando \e[1,93mDocker\e[0m"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Despliegue de \e[95mhello-world\e[0m"
+sudo docker run hello-world
